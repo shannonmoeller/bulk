43c7782b9a4425733ce43812c30babc90ae1e083
Shannon Moeller <me@shannonmoeller.com>
2011-11-18 14:15:48 -0500

    sanitized options, quotes, and conditions

--- bulk.sh
+++ bulk.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # bulk: Do the same thing in multiple places.
 # Author: Shannon Moeller <me@shannonmoeller.com>
+# vim: set filetype=sh:
 
 # aliases
 shopt -s expand_aliases && [[ -f ~/.bashrc ]] && . ~/.bashrc
@@ -8,44 +9,49 @@ shopt -s expand_aliases && [[ -f ~/.bashrc ]] && . ~/.bashrc
 # split
 bulksplit() {
 	# split arguments
-	echo "$@" | tr -s '\n,' '\n' | sed 's/\/$//' | sort -u
+	echo "$@" | tr -s "\n," "\n" | sed "s/\/$//" | sort -u
 }
 
 # variables
 IFS=$'\n'
 dir=$(bulksplit "$(ls -1)")
-off=0
-eol='\n'
-sub=''
-exc=''
-inc=''
+eol="\n"
+sub=""
+exc=""
+inc=""
 
 # get options
-while getopts '1d:e:i:' opt; do case $opt in
-	1) eol=' -> ';    (( off += 1 ));;
-	d) sub="$OPTARG"; (( off += 2 ));;
-	e) exc="$OPTARG"; (( off += 2 ));;
-	i) inc="$OPTARG"; (( off += 2 ));;
-	*) break;;
-esac; done; OPTIND=1
+while getopts "1d:e:i:" opt; do
+	case "$opt" in
+		1) eol=" -> ";;
+		d) sub="$OPTARG";;
+		e) exc="$OPTARG";;
+		i) inc="$OPTARG";;
+	esac
+done
 
-# remove option arguments
-(( $off > 0 )) && shift $off
+# make argv useful again
+shift $(( $OPTIND - 1 ))
 
 # filter excludes
-[[ -n $exc ]] && dir=$(comm -23 <(printf '%s\n' ${dir[@]}) <(bulksplit "$exc"))
+if [[ -n $exc ]]; then
+	dir=$(comm -23 <(printf "%s\n" ${dir[@]}) <(bulksplit "$exc"))
+fi
 
 # filter includes
-[[ -n $inc ]] && dir=$(comm -12 <(printf '%s\n' ${dir[@]}) <(bulksplit "$inc"))
+if [[ -n $inc ]]; then
+	dir=$(comm -12 <(printf "%s\n" ${dir[@]}) <(bulksplit "$inc"))
+fi
 
 # command
-com=$(printf ' %q' "$@")
+com=$(printf " %q" "$@")
 
 # execute
-[[ -n $com ]] && printf '%q\n' ${dir[@]} | while read d; do
+[[ -n $com ]] && printf "%q\n" ${dir[@]} | while read d; do
 	d="$d/$sub"
+
 	if [[ -d $d ]]; then
-		printf '\e[0;1;34;40m./%q\e[33m>%s%b\e[0m' $d $com $eol
-		printf '%s\n' "$(cd $d && eval $com)"
+		printf "\e[0;1;34;40m./%q\e[33m>%s%b\e[0m" $d $com $eol
+		printf "%s\n" "$(cd $d && eval $com)"
 	fi
 done
